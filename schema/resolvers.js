8b9e72ceffd90e5ccaa5fea7bfadd1fe509bfdc7
Mohammad Arif <arif_iq@yahoo.co.in>
2022-07-13 16:00:39 +0530

    Movie List and friends data is added, new resolvers are there with new type definitions.

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,4 +1,5 @@
-const { UserList } = require('../FakeData')
+const { UserList, MovieList } = require('../FakeData')
+const _ = require('lodash')
 
 /**
  * resolvers
@@ -10,8 +11,33 @@ const { UserList } = require('../FakeData')
 
 const resolvers = {
   Query: {
-    users () {
+    // USER RESOLVERS
+    users: () => {
       return UserList
+    },
+    user: (parent, args) => {
+      const id = args.id
+      const user = _.find(UserList, { id: Number(id) })
+      return user
+    },
+
+    // MOVIE RESOLVERS
+    movies: () => {
+      return MovieList
+    },
+    movie: (parent, args) => {
+      const name = args.name
+      const movie = _.find(MovieList, { name })
+      return movie
+    }
+  },
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        movie =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      )
     }
   }
 }
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -9,18 +9,62 @@ const { gql } = require('apollo-server')
  * Required unless you provide schema.
  */
 
+/**
+ * Scalars
+ * Scalars are equivalent to primitive data types in a programming language.
+ * In GraphQL, there are five built-in scalar types:
+ *
+ * 'Boolean', true or false
+ * 'Int', a signed 32‐bit numeric non‐fractional value
+ * 'Float', a signed double‐precision fractional values
+ * 'String', a sequence of UTF‐8 characters
+ * 'ID', a unique identifier
+ *
+ */
+
 const typeDefs = gql`
   type User {
     id: ID!
     name: String!
     username: String!
     age: Int!
-    nationality: String!
+    nationality: Nationality!
+    friends: [User]
+    favoriteMovies: [Movie]
+  }
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   type Query {
     users: [User!]!
+    user(id: ID!): User!
+    movies: [Movie!]!
+    movie(name: String!): Movie!
+  }
+
+  enum Nationality {
+    CANADA
+    BRAZIL
+    INDIA
+    GERMANY
+    CHILE
   }
 `
+
+/**
+ * Enumeration types
+ *
+ * Also called Enums, enumeration types are a special kind of 'scalar'
+ * that is restricted to a particular set of allowed values.
+ *
+ * This allows you to:
+ * 1. Validate that any arguments of this type are one of the allowed values
+ * 2. Communicate through the type system that a field will always be one of a finite set of values
+ */
 
 module.exports = { typeDefs }
